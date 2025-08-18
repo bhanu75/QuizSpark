8e07c6b8fdf57627c7b8af516a9e96db129b8600
Bhanu Tripathi <26444713+bhanu75@users.noreply.github.com>
2025-08-18 06:18:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Explicitly set root
+  publicDir: 'public', // Explicitly set public directory
   server: {
     port: 3000
   },
